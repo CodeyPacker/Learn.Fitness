683c5528a14a5f1f549338a7f59369f397235c6d
Cody Packer <codeypacker@gmail.com>
2021-08-17 02:49:31 -0600

    cleanup - file structure

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Header from "../src/components/Header";
+import Header from "../src/components/header/Header";
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className={`${styles.mainContent}`}>
         <Header />
       </main>
 
