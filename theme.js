20f786f9bef66edd732e98a5e73bb1febf2d89f4
Cody Packer <codeypacker@gmail.com>
2021-08-17 00:02:32 -0600

    added a theme file with colors

---
+++ theme.js
@@ -0,0 +1,21 @@
+const theme = {
+  colors: {
+    blue: "#1b0c69",
+    purple: "#5d2ae5",
+    teal: "#2cfef6",
+    yellow: "#fffd38",
+    pink: "#fc1781",
+    green: "#41d3a2",
+    lightBlue: "#90d4e7",
+    orange: "#f2582c",
+    lightOrange: "#ff9000",
+    darkPink: "#b71178",
+    lightPink: "#f9a7b5",
+    gold: "#fdcd07",
+    gray: "#264450",
+    lightGray: "#b9b9b9",
+    white: "#ffffff",
+  },
+};
+
+export default theme;
